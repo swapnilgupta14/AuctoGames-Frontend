95a6b22d88f5673224870aad11f60312ddd42cd7
swapnilgupta14 <swapnilguptaswag@gmail.com>
2025-01-10 10:15:41 +0530

    update points calc

--- src/pages/AuctionResult/YourTeamPlayers.jsx
+++ src/pages/AuctionResult/YourTeamPlayers.jsx
@@ -66,7 +66,8 @@ const PlayerCard = ({
         <div className="text-red-700">
           <p className="text-sm font-semibold text-gray-600">Points</p>
           <p className="font-semibold">
-            {player?.points?.positions[index + 1] || "0"}
+            {player?.points?.positions[index + 1] + (player?.points ?? 0) ||
+              "0"}
           </p>
         </div>
       </div>
